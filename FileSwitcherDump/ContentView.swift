f851d14f2edd0afdf6658db9edf082c57f58b2fd
Matthew Anisovich <anisovich.matthew@gmail.com>
2023-02-25 20:58:26 +0000

    v1.0

--- FileSwitcherDump/ContentView.swift
+++ FileSwitcherDump/ContentView.swift
@@ -6,14 +6,49 @@
 //
 
 import SwiftUI
+import FilePicker
+import Zip
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image(uiImage: Bundle.main.icon!)
+                .cornerRadius(10)
+            Text("FileSwitcherDump")
+                .font(.title2)
+            
+            Text("Licenses matter")
+                .font(.footnote)
+                .foregroundColor(.secondary)
+                .padding(.bottom)
+            Button {
+                UIApplication.shared.alert(title: "Credits", body: "by sourcelocation, \n\nironically uses:\nhttps://github.com/markrenaud/FilePicker")
+            } label: {
+                Image(systemName: "info.circle")
+            }
+            .padding(.bottom, 64)
+            
+            Button {
+                
+            } label: {
+            }
+            
+            FilePicker(types: [.init(filenameExtension: "fsp")!], allowMultiple: false, onPicked: { urls in
+                guard let url = urls.first else { return }
+                let zipurl = url.deletingPathExtension().appendingPathExtension("zip")
+                try! FileManager.default.moveItem(at: url, to: zipurl)
+                
+                try! Zip.unzipFile(zipurl, destination: FileManager.default.temporaryDirectory.appendingPathComponent("out"), overwrite: true, password: "aVBob25l5oyB44Gj44Gm6LuK5Lit5rOK44Gu5peF44Gr6KGM44GN44Gf44GE44Gq44CC44GC44Gj44Gf44GL44GE44Gf44G+44GU44KC6aOf44G544Gf44GE44GX44CC")
+                
+                let vc = UIActivityViewController(activityItems: [FileManager.default.temporaryDirectory.appendingPathComponent("out")], applicationActivities: nil)
+                UIApplication.shared.windows[0].rootViewController?.present(vc, animated: true)
+            }, label: {
+                Text("Dump .fsp file")
+                    .padding(12)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            })
         }
         .padding()
     }
--- FileSwitcherDump/FileSwitcherDumpApp.swift
+++ FileSwitcherDump/FileSwitcherDumpApp.swift
@@ -15,3 +15,16 @@ struct FileSwitcherDumpApp: App {
         }
     }
 }
+
+// https://stackoverflow.com/a/51241158
+extension Bundle {
+    public var icon: UIImage? {
+        if let icons = infoDictionary?["CFBundleIcons"] as? [String: Any],
+            let primaryIcon = icons["CFBundlePrimaryIcon"] as? [String: Any],
+            let iconFiles = primaryIcon["CFBundleIconFiles"] as? [String],
+            let lastIcon = iconFiles.last {
+            return UIImage(named: lastIcon)
+        }
+        return nil
+    }
+}
