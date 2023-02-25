909c0fd74f8ef2235f65b116f8fa478ea6145b3d
Matthew Anisovich <anisovich.matthew@gmail.com>
2023-02-25 19:56:21 +0000

    Initial Commit

---
+++ FileSwitcherDump/FileSwitcherDumpApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileSwitcherDumpApp.swift
+//  FileSwitcherDump
+//
+//  Created by sourcelocation on 25/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct FileSwitcherDumpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
